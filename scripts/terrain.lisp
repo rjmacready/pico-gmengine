b22e4a164f0798b03579cb00bb1cdb2d25f07d35
RJ MacReady <82rjmacready@gmail.com>
2013-11-20 12:39:38 -0800

    ***

---
+++ scripts/terrain.lisp
@@ -0,0 +1,10 @@
+; this is not a script per se, but a "literal" representation of a terrain
+; 1- obstacle
+; 0- free
+((0 0 1 1 0 0 0)
+ (0 1 0 0 0 1 0)
+ (0 0 0 1 0 1 0)
+ (1 0 1 0 1 0 0)
+ (0 0 0 0 1 1 1)
+ (1 0 0 1 0 0 0)
+ (0 1 0 0 0 0 1))
